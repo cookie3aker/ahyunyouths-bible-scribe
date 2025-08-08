b9d56c4ae23b5b3d6ee0c4014c813062f8a287e4
cookie3aker <cookie3aker@gmail.com>
2025-08-08 11:13:22 +0900

    Fix: 소그룹 조회 API를 퍼블릭으로 변경

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
-import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { group } from "~/server/db/schema";
 
 export const groupRouter = createTRPCRouter({
-  getGroups: protectedProcedure.query(async ({ ctx }) => {
+  getGroups: publicProcedure.query(async ({ ctx }) => {
     const groups = await ctx.db.query.group.findMany({
       orderBy: (group, { asc }) => [asc(group.group_id)],
     });
