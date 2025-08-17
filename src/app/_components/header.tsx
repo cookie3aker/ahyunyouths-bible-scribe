e298f955a72aafafb3cd5528f7048f6c5a462266
cookie3aker <cookie3aker@gmail.com>
2025-08-18 00:49:47 +0900

    Implement: 채우기 페이지 레이아웃 수정

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,17 +2,17 @@ import { auth } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export async function Header() {
-  const session = await auth();
-  const groups = await api.group.getGroups();
-  const myGroupName = session?.user.groupId
-    ? groups.find((g) => g.group_id === session.user.groupId)?.group_name
-    : "";
-
   return (
-    <header className="bg-blue-500 p-4 text-white">
-      <div>
-        {myGroupName} 소그룹 | {session?.user.name}
-      </div>
+    <header className="mt-[60px] w-full">
+      <ul className="flex w-full items-center justify-end gap-[20px]">
+        <li className="text-[14px] font-bold text-[#FFF8F2]">
+          아현젊은이교회 성경 필사
+        </li>
+        <li className="flex h-[31px] w-[31px] items-center justify-center rounded-full bg-[#B5D4E8] text-[14px] font-bold">
+          MY
+        </li>
+        <li className="text-[14px] font-bold">취소</li>
+      </ul>
     </header>
   );
 }
