696b32987efb0a0b9837b670b8b5ef8ce10767ed
cookie3aker <cookie3aker@gmail.com>
2025-08-21 01:52:34 +0900

    Implement: 나눔 글쓰기 성공 모달 구현

---
+++ src/app/(protected)/community/write/_components/success-modal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export function SuccessModal() {
+  const router = useRouter();
+
+  const handleClickComplete = () => {
+    router.push("/community");
+  };
+
+  return (
+    <div className="fixed inset-0 z-9999 flex items-center justify-start bg-black/50 backdrop-blur-sm">
+      <div className="mx-4 w-full max-w-[768px] rounded-[20px] bg-white">
+        <div className="flex h-[190px] flex-col items-center justify-center">
+          <h2 className="mb-[8px] text-[20px] font-bold">은혜 나눔 성공!</h2>
+
+          <p className="mb-[40px] text-[14px] text-[#A1A1A1]">
+            나눠줘서 고마워
+          </p>
+          <button
+            onClick={handleClickComplete}
+            className="h-[44px] w-full max-w-[226px] rounded-[20px] bg-[#91CDF4] text-[14px] font-bold text-[#FFFFFF]"
+          >
+            나눔 보러가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/(protected)/community/write/page.tsx
+++ src/app/(protected)/community/write/page.tsx
@@ -5,6 +5,7 @@ import type { ChangeEvent } from "react";
 import BottomSheet, { type BibleSelection } from "./_components/bottom-sheet";
 import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
+import { SuccessModal } from "./_components/success-modal";
 
 export default function WritePage() {
   const [content, setContent] = useState("");
@@ -13,8 +14,7 @@ export default function WritePage() {
     null,
   );
 
-  const router = useRouter();
-  const { mutateAsync: createPost } = api.post.create.useMutation();
+  const { mutateAsync: createPost, isSuccess } = api.post.create.useMutation();
 
   const handleTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const newValue = e.target.value;
@@ -45,78 +45,81 @@ export default function WritePage() {
         chapterId: selectedBible.chapterId,
         verseId: selectedBible.verseId,
       });
-      router.push("/community");
     } catch (error) {
       console.error("Failed to create post:", error);
     }
   };
 
   return (
-    <main className="flex-grow px-[20px] pb-[130px]">
-      <div className="mb-[30px] w-full">
-        <p className="text-[18px] leading-[34px] font-bold">
-          필사를 하며 은혜를 받았던
-          <br />
-          본문을 고르고 나눔을 써줘!
-        </p>
-      </div>
-
-      <div>
-        <button
-          onClick={handleClickSelectBible}
-          className="flex h-[74px] w-full justify-between rounded-t-[20px] bg-[#CFE3EF]/60 px-[20px] py-[16px]"
-        >
-          <span className="text-[12px] font-bold text-[#4B90BB]">
-            {selectedBible
-              ? `${selectedBible.bookName} ${selectedBible.chapterNumber}:${selectedBible.verseNumber}`
-              : "은혜 받았던 본문을 선택해줘"}
-          </span>
-          <svg
-            width="19"
-            height="11"
-            viewBox="0 0 19 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.5 1L9.5 9L17.5 1"
-              stroke="#72A8CB"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
-        </button>
-
-        <BottomSheet
-          isOpen={isBottomSheetOpen}
-          onClose={() => setIsBottomSheetOpen(false)}
-          onSelect={(selection) => {
-            setSelectedBible(selection);
-            setIsBottomSheetOpen(false);
-          }}
-        />
-
-        <div className="relative mb-[36px]">
-          <textarea
-            rows={1}
-            value={content}
-            maxLength={500}
-            className="h-auto min-h-[120px] w-full -translate-y-[30px] resize-none rounded-[20px] bg-[#FFFFFF] px-[24px] py-[30px] outline-none"
-            onChange={handleTextAreaChange}
-            placeholder="너의 은혜를 나눠줘!"
-          />
-          <div className="absolute right-1 bottom-2 text-sm text-[15px] font-bold">
-            {content.length}/500
-          </div>
+    <>
+      <main className="flex-grow px-[20px] pb-[130px]">
+        <div className="mb-[30px] w-full">
+          <p className="text-[18px] leading-[34px] font-bold">
+            필사를 하며 은혜를 받았던
+            <br />
+            본문을 고르고 나눔을 써줘!
+          </p>
         </div>
 
-        <button
-          onClick={handleClickSave}
-          className="h-[44px] w-full rounded-[20px] bg-[#CFE3EF] text-[14px] font-bold text-[#4B90BB]"
-        >
-          저장하기
-        </button>
-      </div>
-    </main>
+        <div>
+          <button
+            onClick={handleClickSelectBible}
+            className="flex h-[74px] w-full justify-between rounded-t-[20px] bg-[#CFE3EF]/60 px-[20px] py-[16px]"
+          >
+            <span className="text-[12px] font-bold text-[#4B90BB]">
+              {selectedBible
+                ? `${selectedBible.bookName} ${selectedBible.chapterNumber}:${selectedBible.verseNumber}`
+                : "은혜 받았던 본문을 선택해줘"}
+            </span>
+            <svg
+              width="19"
+              height="11"
+              viewBox="0 0 19 11"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.5 1L9.5 9L17.5 1"
+                stroke="#72A8CB"
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+            </svg>
+          </button>
+
+          <BottomSheet
+            isOpen={isBottomSheetOpen}
+            onClose={() => setIsBottomSheetOpen(false)}
+            onSelect={(selection) => {
+              setSelectedBible(selection);
+              setIsBottomSheetOpen(false);
+            }}
+          />
+
+          <div className="relative mb-[36px]">
+            <textarea
+              rows={1}
+              value={content}
+              maxLength={500}
+              className="h-auto min-h-[120px] w-full -translate-y-[30px] resize-none rounded-[20px] bg-[#FFFFFF] px-[24px] py-[30px] outline-none"
+              onChange={handleTextAreaChange}
+              placeholder="너의 은혜를 나눠줘!"
+            />
+            <div className="absolute right-1 bottom-2 text-sm text-[15px] font-bold">
+              {content.length}/500
+            </div>
+          </div>
+
+          <button
+            onClick={handleClickSave}
+            className="h-[44px] w-full rounded-[20px] bg-[#8CBEDE] text-[14px] font-bold text-[#FFFFFF]"
+          >
+            저장하기
+          </button>
+        </div>
+      </main>
+
+      {isSuccess && <SuccessModal />}
+    </>
   );
 }
