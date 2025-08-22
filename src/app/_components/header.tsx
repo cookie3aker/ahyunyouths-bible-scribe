bd5df2f63e98f31be224d286253ce3fbfb1fa17c
cookie3aker <cookie3aker@gmail.com>
2025-08-22 14:22:57 +0900

    Design: MY 버튼 스타일 변경

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -6,7 +6,7 @@ export async function Header() {
       <li className="text-[14px] font-bold text-[#FFF8F2]">
         성경 정주행 - 필사편
       </li>
-      <li className="flex h-[31px] w-[31px] items-center justify-center rounded-full bg-[#B5D4E8] pt-[1px] text-[14px] font-bold">
+      <li className="flex h-[31px] w-[31px] items-center justify-center rounded-full bg-[#B5D4E8] pt-[1px] text-[12px] font-bold text-[#fff]">
         <Link href="/my">MY</Link>
       </li>
       {/* <li className="text-[14px] font-bold">취소</li> */}
